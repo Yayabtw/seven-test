7904e3b9132852a2c546d50ab17acc3cb492c86a
Yanis MERABET <78480186+Yayabtw@users.noreply.github.com>
2024-06-22 10:59:48 +0200

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const vuetify = createVuetify()
+
+const app = createApp(App)
+app.use(pinia)
+app.use(vuetify)
+app.mount('#app')
