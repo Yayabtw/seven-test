7f002abc4b00025f38f3dea85221980804386bef
Yanis MERABET <78480186+Yayabtw@users.noreply.github.com>
2024-06-22 11:21:38 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ import router from './router'
 import { createPinia } from 'pinia'
 import { createVuetify } from 'vuetify'
 import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import '@mdi/font/css/materialdesignicons.css'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const pinia = createPinia()
-const vuetify = createVuetify()
+const vuetify = createVuetify({
+  components,
+  directives
+})
 
 createApp(App).use(router).use(pinia).use(vuetify).mount('#app')
