edbf043cd10bd14fe07e82323bca1b09ee641377
Yanis MERABET <78480186+Yayabtw@users.noreply.github.com>
2024-06-22 13:01:53 +0200

    added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ConversationsList from '@/components/ConversationsList.vue'
+import ConversationMessages from '@/components/ConversationMessages.vue'
+import Login from '@/components/LoginForm.vue'
 
 const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: Login
+  },
   {
     path: '/conversations/:id',
     name: 'ConversationsList',
     component: ConversationsList
+  },
+  {
+    path: '/conversation/:id',
+    name: 'ConversationMessages',
+    component: ConversationMessages
   }
 ]
 
